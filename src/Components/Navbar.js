b94d0674789ed307e54c186200718a4d46073d93
DG143 <goeldeep11@gmail.com>
2024-07-10 18:12:21 +0530

    35

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -15,12 +15,9 @@ export default class Navbar extends Component {
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" to="/">Home</a>
-        </li>
+        <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/general">Home</Link></li>
 
-          <li className="nav-item">
-          <Link className="nav-link" to="/business">business</Link></li>
+          <li className="nav-item"><Link className="nav-link" to="/business">business</Link></li>
           <li className="nav-item"><Link className="nav-link" to="/entertainment">entertainment</Link></li>
           <li className="nav-item"><Link className="nav-link" to="/general">general</Link></li>
           <li className="nav-item"><Link className="nav-link" to="/health">health</Link></li>
